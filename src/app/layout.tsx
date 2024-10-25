32035f10e8eb970384e8bc5b42a9bb755201377a
Maxwell Young <maxtheyoung@gmail.com>
2024-10-25 21:40:08 +1300

    new font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,9 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const authenticSans = localFont({
+  src: "../../public/fonts/authentic-sans-60.woff2",
+  variable: "--font-authentic-sans",
 });
 
 export const metadata: Metadata = {
@@ -25,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${authenticSans.variable} antialiased`}>
         {children}
       </body>
     </html>
